bf9fd1f9ec9610a4ce33f1bbf85de1a2574f661a
lancekasturias <lanceasturias@gmail.com>
2024-09-09 19:48:28 +0800

    final

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -14,6 +14,30 @@ export class App extends Component {
     filter: '',
   };
 
+  componentDidMount() {
+    const saveContacts = localStorage.getItem('contacts');
+
+    if (saveContacts !== null) {
+      this.setState({ contacts: JSON.parse(saveContacts) });
+    } else {
+      this.setState({
+        contacts: [
+          { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+          { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+          { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+          { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+        ],
+        filter: '',
+      });
+    }
+  }
+
+  componentDidUpdate(_prevProps, prevState) {
+    if (prevState.contacts !== this.state.contacts) {
+      localStorage.setItem('contacts', JSON.stringify(this.state.contacts));
+    }
+  }
+
   addContact = newContact => {
     this.setState(prevState => ({
       contacts: [...prevState.contacts, newContact],
